80d534d3e3ccd53bd30f2e503bc59a356441a4d7
DangVuDinhKhang <dangvudinhkhang11092001@gmail.com>
2024-05-12 21:42:28 +0700

    update deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,8 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
   ],
+  app: {
+    baseURL: '/dashboard_iot_fe/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  }
 })
